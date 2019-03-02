5f04ff57697ed600d91d5b59001f7973f69aa6d7
Natanael Adityasatria <nata.adit@gmail.com>
2019-03-02 17:38:25 +0800

    add publisher and subscriber

---
+++ Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = AmqpTcpEndpoint.UseDefaultPort
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    var message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(String.Empty, "hello", null, body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
